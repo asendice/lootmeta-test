a7ce399e367c25e4f6372b54eaf6a2ab1a37d36c
Dylan Travis <dylan.travis23@outlook.com>
2021-06-29 02:24:14 -0700

    api set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-function App() {
+//access token: USLkLUzmuKtQ8jto5E9v6tTokDVSLWK7cc
+
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [term, setTerm] = useState("");
+
+  // get all of the wow items - literally all of them
+  const getItems = async () => {
+    let arrOfItems = [];
+
+    await axios
+      .get(
+        `https://us.api.blizzard.com/data/wow/search/item?namespace=static-us&name.en_US=${term}&orderby=id&_page=1&access_token=USLkLUzmuKtQ8jto5E9v6tTokDVSLWK7cc`
+      )
+      .then((res) => {
+        arrOfItems.push(res.data.results);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+    setItems(arrOfItems);
+  };
+
+  // getting item images
+  // const getItemImages = async () => {
+  //   let arrOfItems = [];
+  //   const itemIds = items.map((item) => {
+  //     return item.data.id;
+  //   });
+  //   console.log(itemIds, "itemIds");
+  //   for (let i = 0; i < itemIds.length - 1; i++) {
+  //     await axios
+  //       .get(
+  //         `https://us.api.blizzard.com/data/wow/media/item/${itemIds[i]}?namespace=static-classic-us&locale=en_US&access_token=USLkLUzmuKtQ8jto5E9v6tTokDVSLWK7cc`
+  //       )
+  //       .then((res) => {
+  //         arrOfItems.push(res.data.results);
+  //       })
+  //       .catch((err) => {
+  //         console.error(err);
+  //       });
+  //   }
+  //   setItemMedia(arrOfItems.flat());
+  // };
+
+  const submitSearch = () => {
+    getItems();
+  };
+
+  console.log(items);
+
+  const renderNamesOfItems = () => {
+    if (items.length > 0) {
+      return items.flat().map((item) => {
+        return (
+          <div key={item.data.id} style={{ border: "solid black 2px" }}>
+            <div>{item.data.name.en_US}</div>
+            <div>{item.data.quality.type}</div>
+            <div>{item.data.item_subclass.name.en_US}</div>
+          </div>
+        );
+      });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input onChange={(e) => setTerm(e.target.value)} />
+      <button onClick={() => submitSearch()}>SUBMIT</button>
+      {renderNamesOfItems()}
     </div>
   );
-}
+};
 
 export default App;
